73f666c0bee2df578e73e5011839178882653f2f
KonradHambuch <konrad.hambuch@gmail.com>
2020-04-17 18:10:28 +0200

    build folder push

---
+++ build/precache-manifest.a6018b580dfd7b7f363829f33eeaa9b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427443cf6db5a753320aceee70e325bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d6ffa822dcee35bab84",
+    "url": "./static/css/main.96635eb4.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6ffa822dcee35bab84",
+    "url": "./static/js/main.71dda965.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
